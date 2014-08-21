4c1c294beefd073dc7ff7d8625a38c33a6ca234b
Alexandr Mashin <sasha77773333@gmail.com>
2014-08-21 10:28:37 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,14 @@ Rails.application.routes.draw do
   match 'about', to: 'static_pages#about', via: :get
   match 'contact', to: 'static_pages#contact', via: :get
   
- resources :users
-  resources :sessions,   only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
-
+  resources :users
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
 end
